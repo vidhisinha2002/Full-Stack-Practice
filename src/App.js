008e273f41bad9bb6bc01ddba8d8f38dc9eb6298
Vidhi Sinha <72182782+vidhisinha2002@users.noreply.github.com>
2022-04-13 12:54:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css';               //we need to import img
+import {useEffect,useState} from 'react';                     //import useState
+
 
 function App() {
+const [userInput1,setUserInput1]=useState({name:"",usn:"",univ:"",gen:"",age:""})   //destructure array
+const [showVal,setShowVal]=useState(false)
+const fn=()=>{
+  if(showVal==true)
+    <h1></h1>
+  else
+    showVal=true
+
+}
+ const handleNameChange1 = (event) => {   
+  const obj1={
+    name:event.target.value,
+    usn:userInput1.usn,
+    univ:userInput1.univ,
+    gen:userInput1.gen,
+    age:userInput1.age
+  }  
+  setUserInput1(obj1) 
+}
+const handleUSNChange1 = (event) => {   
+  const obj1={
+    name:userInput1.name,
+    usn:event.target.value,
+    univ:userInput1.univ,
+    gen:userInput1.gen,
+    age:userInput1.age
+  }   
+  setUserInput1(obj1)  
+}
+const handleUNIVChange1 = (event) => {   
+  const obj1={
+    name:userInput1.name,
+    usn:userInput1.usn,
+    univ:event.target.value,
+    gen:userInput1.gen,
+    age:userInput1.age
+  }  
+  setUserInput1(obj1)   
+}
+const handleGENChange1 = (event) => {   
+  const obj1={
+    name:userInput1.name,
+    usn:userInput1.usn,
+    univ:userInput1.univ,
+    gen:event.target.value,
+    age:userInput1.age
+  }  
+  setUserInput1(obj1)   
+}
+const handleAGEChange1 = (event) => {   
+  const obj1={
+    name:userInput1.name,
+    usn:userInput1.usn,
+    univ:userInput1.univ,
+    gen:userInput1.gen,
+    age:event.target.value
+  }  
+  setUserInput1(obj1)   
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <form>
+    
+    <input id="name"onChange={handleNameChange1} value={userInput1.name}></input><br></br>
+    <input id="usn"onChange={handleUSNChange1}value={userInput1.usn}></input><br></br>
+    <input id="usn"onChange={handleUNIVChange1}value={userInput1.univ}></input><br></br>
+    <input id="usn"onChange={handleGENChange1}value={userInput1.gen}></input><br></br>
+    <input id="usn"onChange={handleAGEChange1}value={userInput1.age}></input><br></br>
+    <button onClick={fn()}></button>
+  </form> 
+
+
     </div>
+   
   );
 }
 
 export default App;
+//camel case
+//{} instead of ""
+//5ip-name usn colg age gender--
